f52d978299b8f3f5dce5785bf989ff2d87ad95ca
Kevin Jiang <kevin51jiang@email.com>
2023-03-03 19:37:20 -0500

    sample code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+
+import { pipeline } from "@xenova/transformers";
+
+// [{label: 'POSITIVE', score: 0.9998176857266375}]
 
 function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+
+  const [classifier, setClassifier] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const pipe = await pipeline("sentiment-analysis");
+      setClassifier(pipe);
+    })();
+  }, []);
+
+  useEffect(() => {
+    (async () => {
+      if (classifier) {
+        const result = await classifier(input);
+        setOutput(result);
+      }
+    })();
+  }, [input]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {classifier ? (
+        <div>
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <div>{output}</div>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
