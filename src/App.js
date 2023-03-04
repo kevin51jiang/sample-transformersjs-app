d96f3c15d0283b9efed47ccc954cb7f90590af73
Kevin Jiang <kevin51jiang@email.com>
2023-03-04 17:35:42 -0500

    fix

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
 
-import { pipeline } from "@xenova/transformers";
+import { pipeline, env } from "@xenova/transformers";
 
 // [{label: 'POSITIVE', score: 0.9998176857266375}]
+
+env.wasm.wasmPaths = "http://localhost:3000/wasm/";
 
 function App() {
   const [input, setInput] = useState("");
@@ -14,30 +16,40 @@ function App() {
 
   useEffect(() => {
     (async () => {
-      const pipe = await pipeline("sentiment-analysis");
-      setClassifier(pipe);
+      const hf_url =
+        "https://huggingface.co/Xenova/transformers.js/resolve/main/quantized";
+      const model_id = "distilbert-base-uncased-finetuned-sst-2-english";
+      const task = "sequence-classification";
+
+      const model_url = `${hf_url}/${model_id}/${task}`;
+      const pipe = await pipeline("sentiment-analysis", model_url);
+      setClassifier(() => pipe);
     })();
   }, []);
 
-  useEffect(() => {
-    (async () => {
-      if (classifier) {
-        const result = await classifier(input);
-        setOutput(result);
-      }
-    })();
-  }, [input]);
+  const classifyText = async () => {
+    if (classifier && input) {
+      const result = await classifier(input);
+      setOutput(result);
+    }
+  };
 
   return (
     <div className="App">
       {classifier ? (
         <div>
+          <p>Input</p>
           <input
             type="text"
             value={input}
             onChange={(e) => setInput(e.target.value)}
           />
-          <div>{output}</div>
+
+          <p>Output</p>
+          <div>{JSON.stringify(output)}</div>
+
+          <br />
+          <button onClick={classifyText}>Classify</button>
         </div>
       ) : (
         <div>Loading...</div>
